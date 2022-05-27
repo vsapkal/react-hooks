01cc7afaf439c29a833f6934b55d7a7411219bc3
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-27 14:37:13 +0530

    useState passing object state

--- src/App.js
+++ src/App.js
@@ -3,25 +3,49 @@ import React, { useState } from 'react'
 
 function App() {
 
-  let initialState = 0;
-  const [count, setCount] = useState(initialState);
+  const [state, setState] = useState({
+    count: 0,
+    theme: 'Blue'
+  });
+
+  let count = state.count;
+  let theme = state.theme;
+
 
   const handleInc = () => {
-    setCount(count + 1);
+    if (count < 0 || count === 0) {
+      setState({
+        count: count + 1,
+        theme: 'red'
+      });
+    } else {
+      setState({
+        count: count + 1,
+        theme: theme
+      });
+    }
   }
 
   const handleDec = () => {
-    if (count !== 0) {
-      setCount(count - 1);
+    setState(count - 1);
+    if (count < 0 || count === 0) {
+      setState({
+        count: count - 1,
+        theme: 'red'
+      });
     } else {
-      setCount(initialState);
+      setState({
+        count: count - 1,
+        theme: theme
+      });
     }
   }
 
   return (
     <div className="App">
       <button className='minus' onClick={handleDec}>-</button>
-      <span>{count}</span>
+      <span>{count} </span>
+      <span>{theme}</span>
       <button className='plus' onClick={handleInc}>+</button>
     </div>
   );
